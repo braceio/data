4c51f588ded870dcc7f3c8a5a3a8b02918c7c152
Cole Krumbholz <cole@brace.io>
2014-12-11 11:50:22 -0500

    a few typos

--- main/__init__.py
+++ main/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from app import create_app
-bd_app = app.create_app()
+main_app = app.create_app()
--- manage.py
+++ manage.py
@@ -3,12 +3,12 @@ from flask.ext.script import Manager
 
 from main import main_app, settings
 
-manager = Manager(bd_app)
+manager = Manager(main_app)
 
 @manager.command
 def run(port=5000):
-    print "debug is %s" % str(data_app.debug)
-    bd_app.run(port=int(port))
+    print "debug is %s" % str(main_app.debug)
+    main_app.run(port=int(port))
 
 if __name__ == "__main__":
     manager.run()
